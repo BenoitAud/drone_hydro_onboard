1523971c28563bdb582118856939e9f3172e17ea
BenoitAud <audetb2@gmail.com>
2025-05-13 23:35:07 -0400

    state may 13th

--- src/main.py
+++ src/main.py
@@ -9,8 +9,6 @@ from brping import Ping1D # blue robotics library, téléchargement github requi
 import sys
 import os
 import glob
-#sys.path.append("/home/pi/SunFounder_SensorKit_for_RPi2/Python/") # bibliotheque pour utilisation capteurs pins GPIO (LCD et temperature)
-#import LCD1602
 from RPLCD.i2c import CharLCD
 from datetime import datetime
 from datetime import timedelta
@@ -18,11 +16,11 @@ from datetime import date
 import serial
 import yaml
 
-# Fonction de conversion F -> C de la température
+# Convert Fahrenheit to Celsius
 def tC_func(tF):
     return((tF-32)*5/9)
 
-# Célérité son en fonction de la température
+# Sound velocity depending on the temperature
 def ss(temperatureC):
     ss = 1404.3+4.7*temperatureC-0.04*temperatureC*temperatureC
     return(1000*ss) #unit = mm/s
@@ -54,11 +52,11 @@ myPing = Ping1D()
 myPing.connect_serial(port_sonar, baud_rate_sonar)
 if myPing.initialize() is False:
     lcd.cursor_pos = (1, 0)
-    lcd.write_string("Sonar init. failed  ")
+    lcd.write_string("Sonar: INIT. FAILED ")
     print("Sonar initialization failed\n")
 else:
     lcd.cursor_pos = (1, 0)
-    lcd.write_string("Sonar ready         ")
+    lcd.write_string("Sonar: READY        ")
     print("Sonar initialized\n")
 
 #   GPS PARAMETERS
@@ -67,11 +65,11 @@ else:
 try:
     ser = serial.Serial(port_gps, baud_rate_gps)
     lcd.cursor_pos = (2, 0)
-    lcd.write_string("GPS ready           ")
+    lcd.write_string("GPS: READY          ")
     print("GPS initialized\n")
 except serial.SerialException as e:
     lcd.cursor_pos = (2, 0)
-    lcd.write_string("GPS init. failed    ")
+    lcd.write_string("GPS: INIT. FAILED   ")
     print(f"Failed to open GPS port: {port_gps}. Error: {e}")
     ser = None  # Set to None to indicate failure
 
@@ -99,7 +97,7 @@ temp_path = device_folder + '/w1_slave'
 # ACQUISITION
 
 # Initialisation des variables
-i_failure = 0 #nombre de fois que le sonar n'arrive pas a se connecter, garder en memoire pour un reboot apres 5 erreurs.
+i_failure = 0 # Sonar connection errors, max 5
 lat_list = []
 long_list = []
 time_list = []
@@ -109,12 +107,12 @@ default_long = 0.000
 
 # Example initialization
 time_init = datetime.now()
-timeout = timedelta(minutes=duree)  # Assuming `duree` is in minutes
+timeout = timedelta(minutes=duree)
 
 # Main loop
 while (datetime.now() - time_init) < timeout and i_failure < 6:
     time_current = datetime.now()
-    tps_ecoule = time_current - time_init
+    elapsed_time = time_current - time_init
 
     # Lecture GPS
     line = ser.readline().decode("utf-8")
@@ -124,17 +122,17 @@ while (datetime.now() - time_init) < timeout and i_failure < 6:
         #print("Not a GPGGA acquisiition, format is %s" %str(words[0]))
         continue
     elif type_GPS == "$GPGGA":
-        print("Temps écoulé (s): ", tps_ecoule.seconds) # mesure de l'avancement
+        print("Temps écoulé (s): ", elapsed_time.seconds) # mesure de l'avancement
         time = datetime.strptime(str(words[1]), "%H%M%S.%f")
         lat_current = str(words[2]+words[3])
         long_current = str(words[4]+words[5])
     elif type_GPS == "$GPGLL": #pas de données position => on prend les valeurs par défaut définies
-        print("Temps écoulé (s): ", tps_ecoule.seconds) # mesure de l'avancement
+        print("Temps écoulé (s): ", elapsed_time.seconds) # mesure de l'avancement
         time = datetime.strptime(str(words[5]), "%H%M%S.%f")
         lat_current = str(default_lat)
         long_current = str(default_long)
     elif type_GPS == "$GPRMC":
-        print("Temps écoulé (s): ", tps_ecoule.seconds) # mesure de l'avancement
+        print("Temps écoulé (s): ", elapsed_time.seconds) # mesure de l'avancement
         time = datetime.strptime(str(words[1]), "%H%M%S.%f")
         lat_current = str(words[3]+words[4])
         long_current = str(words[5]+words[6])
@@ -147,7 +145,7 @@ while (datetime.now() - time_init) < timeout and i_failure < 6:
         time_list.append(time_current)
         lat_list.append(lat_current)
         long_list.append(long_current)
-        chemin_success = "file_" + datetime.now().strftime("%Y-%m-%d_%H%M%S") + ".txt"
+        chemin_success = "file_" + datetime.now().strftime("%Y-%m-%d2_%H%M%S") + ".txt"
 
         f = open(os.path.join(f"{path_logs}/sonar/", chemin_success), "a+") #fichier pour l'enregistrement des données
     
@@ -155,90 +153,97 @@ while (datetime.now() - time_init) < timeout and i_failure < 6:
         print("Value Error on GPS")
         continue
 
-    # # update temperature from sensor
-    # with open(temp_path, "r") as temperature_file:
-    #     lines = f.readlines()
-    #     temp_data = lines[1].split('t=')[-1]
-    #     temp_c = float(temp_data) / 1000.0
-    #     print("water temperature is : %f C" %temp_c)
-
-        # text = temperature_file.read()
-        # i_tC = text.find('t=')
-        # tC = float(text[i_tC+2:])/1000
-        # #print("water temperature is : %f C" %tC)
-
-#     try:
-#         myPing.set_speed_of_sound(int(ss(tC)))
-#         print("Sound velocity updated (mm/s): ", ss(tC))
-#         # get the data
-#         data = myPing.get_distance()
+    # update temperature from sensor
+    with open(temp_path, "r") as temperature_file:
+        lines = temperature_file.readlines()
+        while lines[0].strip()[-3:] != 'YES':
+            time.sleep(0.2)
+            with open(temp_path, "r") as temperature_file_retry:
+                lines = temperature_file_retry.readlines()
         
-#         # erreur 1: le sonar ne renvoie pas de donnees. Tentative de reinitialisation!
-#         if data is None:
-#             LCD1602.write(0, 0, "No data")
-#             print("No data")
-#             print("Reinitialisation")
-#             myPing.connect_serie("/dev/ttyUSB0", 115200)
-#             myPing.initialize()
-#             data = myPing.get_distance()
+        temp_data = lines[1].split('t=')[-1]
+        temp_c = float(temp_data) / 1000.0
+        print(f"water temperature is : {temp_c:.2f} C")
+
+    try:
+        myPing.set_speed_of_sound(int(ss(tC)))
+        print("Sound velocity updated (mm/s): ", ss(tC))
+        # get the data
+        data = myPing.get_distance()
+        
+        # erreur 1: le sonar ne renvoie pas de donnees. Tentative de reinitialisation!
+        if data is None:
+            lcd.cursor_pos = (1, 0)
+            lcd.write_string("Sonar: NO DATA      ")
+            print("No data")
+            print("Reinitialisation")
+            myPing.connect_serial(port_sonar, baud_rate_sonar)
+            myPing.initialize()
+            data = myPing.get_distance()
             
-#             if data is None:
-#                 data = {"distance": -1, "confidence": -1}
-#                 LCD1602.write(0, 1, "Reinit failed")
-#                 print("Reinitialisation failed")
-#                 i_failure += 1
-#             else:
-#                 LCD1602.write(0, 1, "Data received")
-#                 print("Reinitialisation succeeded")
+            if data is None:
+                data = {"distance": -1, "confidence": -1}
+                lcd.cursor_pos = (1, 0)
+                lcd.write_string("Sonar: REINIT FAILED")
+                print("Reinitialisation failed")
+                i_failure += 1
+            else:
+                lcd.cursor_pos = (1, 0)
+                lcd.write_string("Sonar: REINIT OK    ")
+                print("Reinitialisation succeeded")
 
-#             distance_current = str(data["distance"])
-#             confidence_current = str(data["confidence"])
-#             dist_list.append(distance_current)
+            distance_current = str(data["distance"])
+            confidence_current = str(data["confidence"])
+            dist_list.append(distance_current)
 
-#         distance_current = str(data["distance"])
-#         confidence_current = str(data["confidence"])
-#         print("Distance =" + distance_current + " mm \n")
-#         msg = "Depth = " + distance_current + " mm"
-#         LCD1602.write(0, 0, msg)
-#         msg_err = "Conf = " + confidence_current + " %"
-#         LCD1602.write(0, 1, msg_err)
-#         dist_list.append(distance_current)
-#         tiime.sleep(0.5)
-#         LCD1602.clear()
+        distance_current = str(data["distance"])
+        confidence_current = str(data["confidence"])
+        print("Distance =" + distance_current + " mm \n")
 
-#         f.write("%(distance)s, %(date)s, %(lat)s, %(long)s, %(confidence)s\n" % {'distance': distance_current, 'date': time_current,'lat': lat_current, 'long': long_current, 'confidence': confidence_current})
-#         LCD1602.write("Acquisition sauvée :)")
-#         print("Acquisition sauvegardée")
-#         f.close()
-#         print("fichier fermé")
+        msg = "Depth = " + distance_current + " mm"
+        lcd.cursor_pos = (3, 0)
+        lcd.write_string(msg)
 
-#     # Deuxieme issue: sonar ne repond plus.
-#     except serial.SerialException:
-#         print("Sonar ne repond plus")
-#         data = {"distance": -1, "confidence": -1}
-#         distance_current = str(data["distance"])
-#         confidence_current = str(data["confidence"])
-#         LCD1602.clear()
-#         LCD1602.write(0, 1, "Acqui failed")
-#         print("Acquisition failed")
-#         i_failure += 1
-#         f.write("%(distance)s, %(date)s, %(lat)s, %(long)s, %(confidence)s\n" % {'distance': distance_current, 'date': time_current,'lat':
-#         lat_current, 'long': long_current, 'confidence': confidence_current})
-#         LCD1602.write("Acquisition sauvée :)")
-#         continue
+        msg_err = "Conf = " + confidence_current + " %"
+        lcd.cursor_pos = (3, 8)
+        lcd.write_string(msg)
 
-# #rebooter et relancer au bout de 5 erreurs
-# if i_failure > 5:
-#     chemin = "fail_" + str(time_current) + ".txt"
-#     print("Rebooting")
-#     LCD1602.clear()
-#     LCD1602.write(0, 0, "Rebooting now")
-#     tiime.sleep(0.5)
-#     os.system("sudo reboot")
+        dist_list.append(distance_current)
+        tiime.sleep(0.5)
+        # TODO clear LCD?
 
-# ####################################################### Sauvegarde et fin
-# ###########################################################################
-# ########
-# LCD1602.clear()
-# LCD1602.write(0,0,"Fin")
-# print("Fin de l'acquisition")
+        f.write("%(distance)s, %(date)s, %(lat)s, %(long)s, %(confidence)s\n" % {'distance': distance_current, 'date': time_current,'lat': lat_current, 'long': long_current, 'confidence': confidence_current})
+        lcd.cursor_pos = (3, 0)
+        lcd.write_string("---- File saved ----")
+        print("Acquisition sauvegardée")
+        f.close()
+        print("fichier fermé")
+
+    # Deuxieme issue: sonar ne repond plus.
+    except serial.SerialException:
+        print("Sonar ne repond plus")
+        data = {"distance": -1, "confidence": -1}
+        distance_current = str(data["distance"])
+        confidence_current = str(data["confidence"])
+        lcd.cursor_pos = (1, 0)
+        lcd.write_string("Sonar: NO DATA      ")
+        print("Acquisition failed")
+        i_failure += 1
+        f.write("%(distance)s, %(date)s, %(lat)s, %(long)s, %(confidence)s\n" % {'distance': distance_current, 'date': time_current,'lat':
+        lat_current, 'long': long_current, 'confidence': confidence_current})
+        lcd.cursor_pos = (3, 0)
+        lcd.write_string("---- File saved ----")
+        continue
+
+#rebooter et relancer au bout de 5 erreurs
+if i_failure > 5:
+    chemin = "fail_" + str(time_current) + ".txt"
+    print("Rebooting")
+    lcd.cursor_pos = (3, 0)
+    lcd.write_string("Rebooting...        ")
+    tiime.sleep(0.5)
+    #os.system("sudo reboot") TODO NECESSARY?
+
+lcd.cursor_pos = (3, 0)
+lcd.write_string("---- Done ----")
+print("Fin de l'acquisition")
